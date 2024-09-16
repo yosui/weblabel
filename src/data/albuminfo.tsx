0d89ad38e07f7cb7d970b701fc33dd783f49d2b5
yosui <yosui262@gmail.com>
2024-09-16 11:04:48 +0200

    updated imgs

--- src/data/albuminfo.tsx
+++ src/data/albuminfo.tsx
@@ -24,19 +24,19 @@ type Album = {
     {
       name: "Weblabel01",
       description: "",
-      image: "../assets/image/track/weblabel01.png",
-      torrent: "../assets/torrents/Weblabel01.torrent",
+      image: "assets/image/track/weblabel01.png",
+      torrent: "assets/torrents/Weblabel01.torrent",
       tracks: [
         {
           name: "Pico Pico",
           artist: "Galaxy Semiconductor",
-          previewUrl: "../assets/audio/pico_pico.mp3",
+          previewUrl: "assets/audio/pico_pico.mp3",
         },
       ],
       artist: [
         {
           artist: "Galaxy Semiconductor",           
-          artistImg: "../assets/image/artist/galaxysemiconductor.png",
+          artistImg: "assets/image/artist/galaxysemiconductor.png",
           donation1: "eth: 0x1dd010A59Ff93e47a7cd865803c6E6b72a757629",       
           donation2: "",  
         },
